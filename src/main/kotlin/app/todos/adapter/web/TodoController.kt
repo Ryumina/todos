ab18a83bd4f60be2fa5f9123eb0f63cd9387ec26
ryumina <alsk464@gmail.com>
2025-10-11 17:59:51 +0900

    Test: TodoController post() 테스트코드 추가

--- src/main/kotlin/app/todos/adapter/web/TodoController.kt
+++ src/main/kotlin/app/todos/adapter/web/TodoController.kt
@@ -1,6 +1,9 @@
 package app.todos.adapter.web
 
+import app.todos.adapter.web.dto.TodoRequestDto
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -10,6 +13,11 @@ class TodoController {
 
     @GetMapping("/")
     fun get(): String {
+        return "hello todos!"
+    }
+
+    @PostMapping("/")
+    fun create(@RequestBody todoRequest: TodoRequestDto): String {
         return "hello todos!"
     }
 }
--- src/test/kotlin/app/todos/adapter/web/TodoControllerTest.kt
+++ src/test/kotlin/app/todos/adapter/web/TodoControllerTest.kt
@@ -3,8 +3,10 @@ package app.todos.adapter.web
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
@@ -19,6 +21,37 @@ class TodoControllerTest {
     fun `todos get 요청시 hello todos!를 출력한다`() {
         mockMvc.perform(get("/api/todos/"))
             .andExpect(status().isOk)
+            .andExpect(content().string("hello todos!"))
+            .andDo(print())
+    }
+
+    @Test
+    fun `todos post 요청시 title이 빈값이면 400 리턴`() {
+        mockMvc.perform(
+            post("/api/todos/")
+                .content("{\"title\": \"  \"}")
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().is4xxClientError)
+            .andDo(print())
+    }
+
+    @Test
+    fun `todos post 요청시 title이 null이면 400 리턴`() {
+        mockMvc.perform(
+            post("/api/todos/")
+                .content("{\"title\": null}")
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().is4xxClientError)
+            .andDo(print())
+    }
+
+    @Test
+    fun `todos post 요청시 hello todos!를 출력한다`() {
+        mockMvc.perform(
+            post("/api/todos/")
+                .content("{\"title\": \"TDD 공부하기\"}")
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk)
             .andExpect(content().string("hello todos!"))
             .andDo(print())
     }
